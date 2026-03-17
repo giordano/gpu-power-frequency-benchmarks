bd6f8210f4e1c89b24a37695ee3758cfe83cd0d7
Mosè Giordano <m.giordano@ucl.ac.uk>
2026-03-17 17:02:38 +0000

    Add BabelStream benchmarks

---
+++ babelstream-benchmarks/analsys.jl
@@ -0,0 +1,80 @@
+using JSON: JSON
+using Plots: heatmap, savefig
+
+benchmarks = filter(d -> startswith(basename(d), r"energy-time-\d+-\d+"), readdir(@__DIR__; join=true))
+
+frequencies = parse.(Float64, getindex.(split.(first.(splitext.(basename.(benchmarks))), '-'), 3))
+frequencies_t = sort!(unique(frequencies))
+powers = parse.(Float64, getindex.(split.(first.(splitext.(basename.(benchmarks))), '-'), 4))
+powers_t = sort!(unique(powers))
+
+times = [JSON.parsefile(ben)["time"] for ben in benchmarks]
+bandwidths = [JSON.parsefile(ben)["bandwidth"] for ben in benchmarks]
+energies = [JSON.parsefile(ben)["energy"] for ben in benchmarks]
+energy_fractions = energies ./ (powers ./ (3600 ./ times)) .* 100
+
+name = "BabelStream"
+bandwidths_plot = heatmap(
+    frequencies_t, powers_t, reshape(bandwidths, length(powers_t), length(frequencies_t));
+    title="$(name) on Nvidia A100",
+    xlabel="GPU frequency (MHz)",
+    xticks=frequencies_t,
+    ylabel="Power cap (W)",
+    yticks=powers_t,
+    colorbar_title="Bandwidth (GB/s)",
+    size=(1000, 1000),
+)
+
+savefig(bandwidths_plot, "bandwidths.png")
+
+energies_plot = heatmap(
+    frequencies_t, powers_t, reshape(energies, length(powers_t), length(frequencies_t));
+    title="$(name) on Nvidia A100",
+    xlabel="GPU frequency (MHz)",
+    xticks=frequencies_t,
+    ylabel="Power cap (W)",
+    yticks=powers_t,
+    colorbar_title="Energy usage (W⋅h)",
+    size=(1000, 1000),
+)
+
+savefig(energies_plot, "energies.png")
+
+bandwidths_energies_plot = heatmap(
+    frequencies_t, powers_t, reshape(bandwidths .* energies, length(powers_t), length(frequencies_t));
+    title="$(name) on Nvidia A100",
+    xlabel="GPU frequency (MHz)",
+    xticks=frequencies_t,
+    ylabel="Power cap (W)",
+    yticks=powers_t,
+    colorbar_title="Bandwidth * energy usage (GB⋅W⋅h/s)",
+    size=(1000, 1000),
+)
+
+savefig(bandwidths_energies_plot, "bandwidths_energies.png")
+
+bandwidths_over_energies_plot = heatmap(
+    frequencies_t, powers_t, reshape(bandwidths ./ energies, length(powers_t), length(frequencies_t));
+    title="$(name) on Nvidia A100",
+    xlabel="GPU frequency (MHz)",
+    xticks=frequencies_t,
+    ylabel="Power cap (W)",
+    yticks=powers_t,
+    colorbar_title="Bandwidth / energy usage ((GB/s)/(W⋅h))",
+    size=(1000, 1000),
+)
+
+savefig(bandwidths_over_energies_plot, "bandwidths_over_energies.png")
+
+energy_fractions_plot = heatmap(
+    frequencies_t, powers_t, reshape(energy_fractions, length(powers_t), length(frequencies_t));
+    title="$(name) on Nvidia A100",
+    xlabel="GPU frequency (MHz)",
+    xticks=frequencies_t,
+    ylabel="Power cap (W)",
+    yticks=powers_t,
+    colorbar_title="Fraction of max energy usage (%)",
+    size=(1000, 1000),
+)
+
+savefig(energy_fractions_plot, "energy_fractions.png")
